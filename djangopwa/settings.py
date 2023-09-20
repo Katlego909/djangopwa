23b1bb77ec13e92a2ed8015e65eab8e5183dd923
Katlego909 <67097039+Katlego909@users.noreply.github.com>
2023-09-20 16:22:10 +0200

    Add files via upload

--- djangopwa/settings.py
+++ djangopwa/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-#8*fiwgylf-)8_cn42f_%e2jsqm#o8)$lifb38oslz+nk5n=%*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app']
 
 # Application definition
 
